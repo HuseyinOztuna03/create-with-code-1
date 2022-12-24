e789acc07f07e11ea7867ac6e9210f6d5acf3cc9
Hüseyin <103373602+HuseyinOztuna03@users.noreply.github.com>
2022-12-24 21:47:53 +0300

    change code

--- Unity Learn I/Assets/Scripts/CarController.cs
+++ Unity Learn I/Assets/Scripts/CarController.cs
@@ -2,20 +2,18 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float forwardInput;
+    private float horizontalInput;
+    private float forwardInput;
 
-    public float turnSpeed;
-    public float forwardForce = 20f;
+    private float turnSpeed = 35f;
+    private float forwardForce = 20f;
 
-    void FixedUpdate()
+    void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        
         transform.Translate(transform.forward * Time.deltaTime * forwardForce * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-        
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
