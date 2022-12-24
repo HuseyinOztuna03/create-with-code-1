f636a3a4a2e0b3f1f161db12c9db40195b6b9d4b
Hüseyin <103373602+HuseyinOztuna03@users.noreply.github.com>
2022-12-24 21:20:30 +0300

    add vertical and horizontal input

--- Unity Learn I/Assets/Scripts/CarController.cs
+++ Unity Learn I/Assets/Scripts/CarController.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody rb;
+    public float horizontalInput;
+    public float forwardInput;
 
-    public float forwardForce = 20f; // Adjust this value to control the car's speed
+    public float turnSpeed;
+    public float forwardForce = 20f;
 
     void FixedUpdate()
     {
-        // Apply a constant forward force to the car's Rigidbody
-        rb.transform.Translate(transform.forward * forwardForce * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        
+        transform.Translate(transform.forward * Time.deltaTime * forwardForce * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
     }
 }
